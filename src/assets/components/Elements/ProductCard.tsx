4f2ff326a65031e07102a31ed91aa56aaf7c91bd
Biniam batu <Biniambatu12@gmail.com>
2024-06-07 10:33:13 +0300

    fetching json api to productlist and other some modification

--- src/assets/components/Elements/ProductCard.tsx
+++ src/assets/components/Elements/ProductCard.tsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import { Rating } from "./Rating";
 
 export const ProductCard = ({product}) => {
-   const { name, overview,long_description,price,poster,image_local,rating,in_stock,size,best_seller }= product;
+   const { id,name, overview,long_description,price,poster,image_local,rating,in_stock,size,best_seller }= product;
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <Link to='' className="relative" >
-            <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">{best_seller}</span> 
+        <Link to={`products/${id}`} className="relative" >
+            {best_seller ?(<span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">best_seller</span>) : (<span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded"></span>)} 
             <img className="rounded-t-lg w-full h-64" src={poster} alt='' />
         </Link>
         <div className="p-5">
-            <Link to=''>
+            <Link to={`products/${id}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
             </Link>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
